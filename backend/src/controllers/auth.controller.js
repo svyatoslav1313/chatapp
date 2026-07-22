9a32fb3a6f7dd323e2f142d212c3d41e15189a7d
svyatoslav1313 <svyatoslavkozelchuk@gmail.com>
2026-07-22 23:16:13 +0300

    fix cookies

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -55,6 +55,8 @@ const generateTokens = async (res, user) => {
   res.cookie("refreshToken", refreshToken, {
     maxAge: 30 * 24 * 60 * 60 * 1000,
     httpOnly: true,
+    sameSite: "none",
+    secure: true,
   });
 
   res.send({
