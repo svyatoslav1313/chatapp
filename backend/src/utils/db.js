49c6bb73c1c564f2fc34d263bd4246860d005044
svyatoslav1313 <svyatoslavkozelchuk@gmail.com>
2026-07-22 23:05:24 +0300

    Add SSL dialect options for Neon DB

--- backend/src/utils/db.js
+++ backend/src/utils/db.js
@@ -1,9 +1,15 @@
-import { Sequelize } from 'sequelize';
+import { Sequelize } from "sequelize";
 
 export const client = new Sequelize({
   host: process.env.DB_HOST,
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_DATABASE,
-  dialect: 'postgres',
+  dialect: "postgres",
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    },
+  },
 });
